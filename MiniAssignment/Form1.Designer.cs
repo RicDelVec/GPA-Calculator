80fe463c2091991d89c94a4f91380f6672f86878
RicDelVec <200405510@student.georgianc.on.ca>
2018-12-03 20:11:22 -0500

    Mini Assignment First Commit

---
+++ MiniAssignment/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace MiniAssignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOutputLetter = new System.Windows.Forms.Label();
+            this.lblOutputGrade = new System.Windows.Forms.Label();
+            this.lblOutput1051 = new System.Windows.Forms.Label();
+            this.lblOutput1016 = new System.Windows.Forms.Label();
+            this.lblOutput1002 = new System.Windows.Forms.Label();
+            this.lblOutput1102 = new System.Windows.Forms.Label();
+            this.lblOutput2030 = new System.Windows.Forms.Label();
+            this.lblOutput1030 = new System.Windows.Forms.Label();
+            this.txtComp1051 = new System.Windows.Forms.TextBox();
+            this.txtComm1016 = new System.Windows.Forms.TextBox();
+            this.txtComp1002 = new System.Windows.Forms.TextBox();
+            this.txtComp1102 = new System.Windows.Forms.TextBox();
+            this.txtGned2030 = new System.Windows.Forms.TextBox();
+            this.txtComp1030 = new System.Windows.Forms.TextBox();
+            this.lblComp1051 = new System.Windows.Forms.Label();
+            this.lblComm1016 = new System.Windows.Forms.Label();
+            this.lblComp1002 = new System.Windows.Forms.Label();
+            this.lblComp1102 = new System.Windows.Forms.Label();
+            this.lblGned2030 = new System.Windows.Forms.Label();
+            this.lblComp1030 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOutputLetter
+            // 
+            this.lblOutputLetter.AutoSize = true;
+            this.lblOutputLetter.Location = new System.Drawing.Point(542, 392);
+            this.lblOutputLetter.Name = "lblOutputLetter";
+            this.lblOutputLetter.Size = new System.Drawing.Size(0, 17);
+            this.lblOutputLetter.TabIndex = 42;
+            // 
+            // lblOutputGrade
+            // 
+            this.lblOutputGrade.AutoSize = true;
+            this.lblOutputGrade.Location = new System.Drawing.Point(258, 392);
+            this.lblOutputGrade.Name = "lblOutputGrade";
+            this.lblOutputGrade.Size = new System.Drawing.Size(0, 17);
+            this.lblOutputGrade.TabIndex = 41;
+            // 
+            // lblOutput1051
+            // 
+            this.lblOutput1051.AutoSize = true;
+            this.lblOutput1051.Location = new System.Drawing.Point(542, 285);
+            this.lblOutput1051.Name = "lblOutput1051";
+            this.lblOutput1051.Size = new System.Drawing.Size(0, 17);
+            this.lblOutput1051.TabIndex = 40;
+            // 
+            // lblOutput1016
+            // 
+            this.lblOutput1016.AutoSize = true;
+            this.lblOutput1016.Location = new System.Drawing.Point(542, 234);
+            this.lblOutput1016.Name = "lblOutput1016";
+            this.lblOutput1016.Size = new System.Drawing.Size(0, 17);
+            this.lblOutput1016.TabIndex = 39;
+            // 
+            // lblOutput1002
+            // 
+            this.lblOutput1002.AutoSize = true;
+            this.lblOutput1002.Location = new System.Drawing.Point(542, 187);
+            this.lblOutput1002.Name = "lblOutput1002";
+            this.lblOutput1002.Size = new System.Drawing.Size(0, 17);
+            this.lblOutput1002.TabIndex = 38;
+            // 
+            // lblOutput1102
+            // 
+            this.lblOutput1102.AutoSize = true;
+            this.lblOutput1102.Location = new System.Drawing.Point(542, 142);
+            this.lblOutput1102.Name = "lblOutput1102";
+            this.lblOutput1102.Size = new System.Drawing.Size(0, 17);
+            this.lblOutput1102.TabIndex = 37;
+            // 
+            // lblOutput2030
+            // 
+            this.lblOutput2030.AutoSize = true;
+            this.lblOutput2030.Location = new System.Drawing.Point(542, 94);
+            this.lblOutput2030.Name = "lblOutput2030";
+            this.lblOutput2030.Size = new System.Drawing.Size(0, 17);
+            this.lblOutput2030.TabIndex = 36;
+            // 
+            // lblOutput1030
+            // 
+            this.lblOutput1030.AutoSize = true;
+            this.lblOutput1030.Location = new System.Drawing.Point(542, 46);
+            this.lblOutput1030.Name = "lblOutput1030";
+            this.lblOutput1030.Size = new System.Drawing.Size(0, 17);
+            this.lblOutput1030.TabIndex = 35;
+            // 
+            // txtComp1051
+            // 
+            this.txtComp1051.Location = new System.Drawing.Point(384, 280);
+            this.txtComp1051.Name = "txtComp1051";
+            this.txtComp1051.Size = new System.Drawing.Size(111, 22);
+            this.txtComp1051.TabIndex = 34;
+            // 
+            // txtComm1016
+            // 
+            this.txtComm1016.Location = new System.Drawing.Point(384, 229);
+            this.txtComm1016.Name = "txtComm1016";
+            this.txtComm1016.Size = new System.Drawing.Size(111, 22);
+            this.txtComm1016.TabIndex = 33;
+            // 
+            // txtComp1002
+            // 
+            this.txtComp1002.Location = new System.Drawing.Point(384, 182);
+            this.txtComp1002.Name = "txtComp1002";
+            this.txtComp1002.Size = new System.Drawing.Size(111, 22);
+            this.txtComp1002.TabIndex = 32;
+            // 
+            // txtComp1102
+            // 
+            this.txtComp1102.Location = new System.Drawing.Point(384, 137);
+            this.txtComp1102.Name = "txtComp1102";
+            this.txtComp1102.Size = new System.Drawing.Size(111, 22);
+            this.txtComp1102.TabIndex = 31;
+            // 
+            // txtGned2030
+            // 
+            this.txtGned2030.Location = new System.Drawing.Point(384, 89);
+            this.txtGned2030.Name = "txtGned2030";
+            this.txtGned2030.Size = new System.Drawing.Size(111, 22);
+            this.txtGned2030.TabIndex = 30;
+            // 
+            // txtComp1030
+            // 
+            this.txtComp1030.Location = new System.Drawing.Point(384, 41);
+            this.txtComp1030.Name = "txtComp1030";
+            this.txtComp1030.Size = new System.Drawing.Size(111, 22);
+            this.txtComp1030.TabIndex = 29;
+            // 
+            // lblComp1051
+            // 
+            this.lblComp1051.AutoSize = true;
+            this.lblComp1051.Location = new System.Drawing.Point(258, 280);
+            this.lblComp1051.Name = "lblComp1051";
+            this.lblComp1051.Size = new System.Drawing.Size(80, 17);
+            this.lblComp1051.TabIndex = 28;
+            this.lblComp1051.Text = "COMP1051";
+            // 
+            // lblComm1016
+            // 
+            this.lblComm1016.AutoSize = true;
+            this.lblComm1016.Location = new System.Drawing.Point(258, 229);
+            this.lblComm1016.Name = "lblComm1016";
+            this.lblComm1016.Size = new System.Drawing.Size(82, 17);
+            this.lblComm1016.TabIndex = 27;
+            this.lblComm1016.Text = "COMM1016";
+            // 
+            // lblComp1002
+            // 
+            this.lblComp1002.AutoSize = true;
+            this.lblComp1002.Location = new System.Drawing.Point(258, 182);
+            this.lblComp1002.Name = "lblComp1002";
+            this.lblComp1002.Size = new System.Drawing.Size(80, 17);
+            this.lblComp1002.TabIndex = 26;
+            this.lblComp1002.Text = "COMP1002";
+            // 
+            // lblComp1102
+            // 
+            this.lblComp1102.AutoSize = true;
+            this.lblComp1102.Location = new System.Drawing.Point(258, 137);
+            this.lblComp1102.Name = "lblComp1102";
+            this.lblComp1102.Size = new System.Drawing.Size(80, 17);
+            this.lblComp1102.TabIndex = 25;
+            this.lblComp1102.Text = "COMP1102";
+            // 
+            // lblGned2030
+            // 
+            this.lblGned2030.AutoSize = true;
+            this.lblGned2030.Location = new System.Drawing.Point(258, 89);
+            this.lblGned2030.Name = "lblGned2030";
+            this.lblGned2030.Size = new System.Drawing.Size(80, 17);
+            this.lblGned2030.TabIndex = 24;
+            this.lblGned2030.Text = "GNED2030";
+            // 
+            // lblComp1030
+            // 
+            this.lblComp1030.AutoSize = true;
+            this.lblComp1030.Location = new System.Drawing.Point(258, 41);
+            this.lblComp1030.Name = "lblComp1030";
+            this.lblComp1030.Size = new System.Drawing.Size(80, 17);
+            this.lblComp1030.TabIndex = 23;
+            this.lblComp1030.Text = "COMP1030";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(384, 330);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(111, 32);
+            this.btnCalculate.TabIndex = 22;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOutputLetter);
+            this.Controls.Add(this.lblOutputGrade);
+            this.Controls.Add(this.lblOutput1051);
+            this.Controls.Add(this.lblOutput1016);
+            this.Controls.Add(this.lblOutput1002);
+            this.Controls.Add(this.lblOutput1102);
+            this.Controls.Add(this.lblOutput2030);
+            this.Controls.Add(this.lblOutput1030);
+            this.Controls.Add(this.txtComp1051);
+            this.Controls.Add(this.txtComm1016);
+            this.Controls.Add(this.txtComp1002);
+            this.Controls.Add(this.txtComp1102);
+            this.Controls.Add(this.txtGned2030);
+            this.Controls.Add(this.txtComp1030);
+            this.Controls.Add(this.lblComp1051);
+            this.Controls.Add(this.lblComm1016);
+            this.Controls.Add(this.lblComp1002);
+            this.Controls.Add(this.lblComp1102);
+            this.Controls.Add(this.lblGned2030);
+            this.Controls.Add(this.lblComp1030);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOutputLetter;
+        private System.Windows.Forms.Label lblOutputGrade;
+        private System.Windows.Forms.Label lblOutput1051;
+        private System.Windows.Forms.Label lblOutput1016;
+        private System.Windows.Forms.Label lblOutput1002;
+        private System.Windows.Forms.Label lblOutput1102;
+        private System.Windows.Forms.Label lblOutput2030;
+        private System.Windows.Forms.Label lblOutput1030;
+        private System.Windows.Forms.TextBox txtComp1051;
+        private System.Windows.Forms.TextBox txtComm1016;
+        private System.Windows.Forms.TextBox txtComp1002;
+        private System.Windows.Forms.TextBox txtComp1102;
+        private System.Windows.Forms.TextBox txtGned2030;
+        private System.Windows.Forms.TextBox txtComp1030;
+        private System.Windows.Forms.Label lblComp1051;
+        private System.Windows.Forms.Label lblComm1016;
+        private System.Windows.Forms.Label lblComp1002;
+        private System.Windows.Forms.Label lblComp1102;
+        private System.Windows.Forms.Label lblGned2030;
+        private System.Windows.Forms.Label lblComp1030;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ MiniAssignment/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniAssignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Input Grades
+            int COMP1030 = 0;
+            bool is1030 = Int32.TryParse(txtComp1030.Text, out COMP1030);
+            int GNED2030 = 0;
+            bool is2030 = Int32.TryParse(txtGned2030.Text, out GNED2030);
+            int COMP1102 = 0;
+            bool is1102 = Int32.TryParse(txtComp1102.Text, out COMP1102);
+            int COMP1002 = 0;
+            bool is1002 = Int32.TryParse(txtComp1002.Text, out COMP1002);
+            int COMM1016 = 0;
+            bool is1016 = Int32.TryParse(txtComm1016.Text, out COMM1016);
+            int COMP1051 = 0;
+            bool is1051 = Int32.TryParse(txtComp1051.Text, out COMP1051);
+
+            //Input Array
+            int[] grades = { COMP1030, GNED2030, COMP1102, COMP1002, COMM1016, COMP1051 };
+
+            //Grades Validation
+            if (grades[0] > 0 && grades[1] > 0 && grades[2] > 0 && grades[3] > 0 && grades[4] > 0 && grades[5] > 0)            
+            {
+                int sum = 0;
+                for (int i = 0; i < grades.Length; i++)
+                    {
+                        sum += grades[i];
+                    }
+
+                //Calculating Average
+                double average = sum / 6;
+
+                //Display Sum
+                lblOutputGrade.Text = "Your Global Average is: " + average;
+
+                //Decision Letter Grade COMP1030
+                if (COMP1030 >= 80)
+                    lblOutput1030.Text = "A";
+                else if (COMP1030 >= 70)
+                    lblOutput1030.Text = "B";
+                else if (COMP1030 >= 60)
+                    lblOutput1030.Text = "C";
+                else if (COMP1030 >= 50)
+                    lblOutput1030.Text = "D";
+                else
+                    lblOutput1030.Text = "F";
+
+                //Decision Letter Grade GNED2030
+                if (GNED2030 >= 80)
+                    lblOutput2030.Text = "A";
+                else if (GNED2030 >= 70)
+                    lblOutput2030.Text = "B";
+                else if (GNED2030 >= 60)
+                    lblOutput2030.Text = "C";
+                else if (GNED2030 >= 50)
+                    lblOutput2030.Text = "D";
+                else
+                    lblOutput2030.Text = "F";
+
+                //Decision Letter Grade COMP1102
+                if (COMP1102 >= 80)
+                    lblOutput1102.Text = "A";
+                else if (COMP1102 >= 70)
+                    lblOutput1102.Text = "B";
+                else if (COMP1102 >= 60)
+                    lblOutput1102.Text = "C";
+                else if (COMP1102 >= 50)
+                    lblOutput1102.Text = "D";
+                else
+                    lblOutput1102.Text = "F";
+
+                //Decision Letter Grade COMP1002
+                if (COMP1002 >= 80)
+                    lblOutput1002.Text = "A";
+                else if (COMP1002 >= 70)
+                    lblOutput1002.Text = "B";
+                else if (COMP1002 >= 60)
+                    lblOutput1002.Text = "C";
+                else if (COMP1002 >= 50)
+                    lblOutput1002.Text = "D";
+                else
+                    lblOutput1002.Text = "F";
+
+                //Decision Letter Grade COMM1016
+                if (COMM1016 >= 80)
+                    lblOutput1016.Text = "A";
+                else if (COMM1016 >= 70)
+                    lblOutput1016.Text = "B";
+                else if (COMM1016 >= 60)
+                    lblOutput1016.Text = "C";
+                else if (COMM1016 >= 50)
+                    lblOutput1016.Text = "D";
+                else
+                    lblOutput1016.Text = "F";
+
+                //Decision Letter Grade COMP1051
+                if (COMP1051 >= 80)
+                    lblOutput1051.Text = "A";
+                else if (COMP1051 >= 70)
+                    lblOutput1051.Text = "B";
+                else if (COMP1051 >= 60)
+                    lblOutput1051.Text = "C";
+                else if (COMP1051 >= 50)
+                    lblOutput1051.Text = "D";
+                else
+                    lblOutput1051.Text = "F";
+
+                //Decision Letter Grade average
+                if (average >= 80)
+                    lblOutputLetter.Text = "A";
+                else if (average >= 70)
+                    lblOutputLetter.Text = "B";
+                else if (average >= 60)
+                    lblOutputLetter.Text = "C";
+                else if (average >= 50)
+                    lblOutputLetter.Text = "D";
+                else
+                    lblOutputLetter.Text = "F";
+            }
+            else
+            {
+                Error();
+            }
+        }
+
+        private void Error()
+        {
+            MessageBox.Show("Invalid entry. Try again.");
+        }
+    }
+}
